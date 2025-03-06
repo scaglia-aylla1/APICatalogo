de507f9b6ca5a1551bf8d4d5981e97d480359e21
Aylla Scaglia <aylla@scaglia.com.br>
2025-03-06 15:44:16 -0300

    Config: pacotes, banco de dados e conexão

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Catalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
